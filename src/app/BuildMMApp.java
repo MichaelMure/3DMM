e18e6c81a2eaaa9aef0f89f127e4e0e5ac2163de
Michael Muré <batolettre@gmail.com>
2012-03-16 12:32:37 +0900

    add an app to test the building of a morphable model

---
+++ src/app/BuildMMApp.java
@@ -0,0 +1,17 @@
+package app;
+
+import gui.Simple3DGUI;
+import io.Importer.FileType;
+import model.MorphableModel;
+import model.MorphableModelBuilder;
+
+public class BuildMMApp {
+
+	public static void main(String[] args) {
+		MorphableModel mm = MorphableModelBuilder.LoadDirectory("data", FileType.PLY);
+		System.out.println(mm);
+		
+		Simple3DGUI gui = new Simple3DGUI();
+		gui.displayRotatingShape(mm.getAverage().getShape3D());
+	}
+}
