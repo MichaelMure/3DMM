ede49c0041eb8eb0bd65103b298ea92a856b020b
Michael Muré <batolettre@gmail.com>
2012-04-19 19:59:41 +0900

    TwoComplement: clamp and round floating point value to avoid error

--- src/util/TwoComplement.java
+++ src/util/TwoComplement.java
@@ -4,19 +4,26 @@ public class TwoComplement {
 
 	public static double to2complement(byte b) {
 		if(b < 0)
-			//return (double) (0x00FF & b);
-			return (double) ((short) b + (short) 256);
-		return (double) b;
+			return (b + (short) 256);
+
+		return b;
 	}
-	
+
 	public static byte from2complement(double d) {
-		short s = (short) d;
+		if(d > 256.0)
+			d = 256.0;
+
+		if(d < 0.0)
+			d = 0.0;
+
+		short s = (short) Math.round(d);
 		if(s < 128)
-			//return (byte) (s - 127);
-			//return (byte) (~(0x00FF & s) + 1);
 			return (byte) s;
+
 		return (byte) (s - 256);
-		
-		
+
+
 	}
 }
+
+// <TITLE>
