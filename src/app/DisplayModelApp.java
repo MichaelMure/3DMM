5bd60221a87de7cb75f16446fa20afaee80a3dc3
Michael Muré <batolettre@gmail.com>
2012-05-08 17:15:04 +0900

    DisplayModelApp: port to JME

--- src/app/DisplayModelApp.java
+++ src/app/DisplayModelApp.java
@@ -1,29 +1,39 @@
 package app;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import model.Model;
+
+import com.jme3.scene.Mesh;
+
 import gui.Simple3DGUI;
 import io.Importer;
 import io.ply.PlyImporter;
 
-import javax.media.j3d.Shape3D;
-
 public class DisplayModelApp {
 
 	public static void main(String[] args) {
+		Logger.getLogger("").setLevel(Level.WARNING);
 		Simple3DGUI gui = new Simple3DGUI();
+		gui.run();
 
-		//ObjImporter importer = new ObjImporter();
+
 		Importer importer = new PlyImporter();
 
-		Shape3D face;
 		try {
+			Mesh face = importer.loadObject("data/00017_20061201_00812_neutral_face05_BAD.ply");
+			gui.displayUnshaded(new Model(face));
+
 			//face = importer.LoadObject("data/00001_20061015_00418_neutral_face05.obj");
 			//face = importer.loadObject("data/face_blender.obj");
-			face = importer.loadObject("data/00293_20080104_03413_neutral_face05.ply");
-			gui.displayRotatingShape(face);
-			gui.run();
+			//face = importer.loadObject("data/00293_20080104_03413_neutral_face05.ply");
+			//gui.displayRotatingShape(face);
+			//gui.run();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+
 	}
 
 }
