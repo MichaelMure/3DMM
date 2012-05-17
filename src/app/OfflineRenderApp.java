affa639d892e92fae3e87051b354e12e61c33ac9
Michael Muré <batolettre@gmail.com>
2012-05-17 14:37:00 +0900

    OfflineRenderApp: port to JME

--- src/app/OfflineRenderApp.java
+++ src/app/OfflineRenderApp.java
@@ -1,8 +1,13 @@
 package app;
 
+import io.Importer;
+import io.ply.PlyImporter;
+
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
@@ -10,28 +15,50 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.WindowConstants;
 
-import render.SceneRenderer;
+import com.jme3.scene.Mesh;
+
+import render.FittingRenderer;
+import render.FittingScene;
+import render.RenderParameter;
 import util.Log;
 import util.Log.LogType;
 
-public class OfflineRenderApp {
+public class OfflineRenderApp implements FittingRenderer.Callback {
 
-	public static void main(String[] args) {
-		BufferedImage target = null;
+	private FittingRenderer renderer;
+
+	public OfflineRenderApp(String[] args) {
 		Log.info(LogType.APP, "Loading image: " + args[0]);
+
+		Importer importer = new PlyImporter();
+
 		try {
-			target = ImageIO.read(new File(args[0]));
+			Mesh face = importer.loadObject("data/00017_20061201_00812_neutral_face05_BAD.ply");
+			BufferedImage target = ImageIO.read(new File(args[0]));
+			FittingScene scene = new FittingScene(face, new RenderParameter());
+
+			renderer = new FittingRenderer(this, scene, target);
+			renderer.setShowSettings(false);
+			renderer.start();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
 
-		SceneRenderer renderer = new SceneRenderer(target);
+	@Override
+	public void rendererCallback() {
 		BufferedImage result = renderer.getRender();
+		renderer.stop();
 
 		JFrame frame = new JFrame();
 		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		frame.getContentPane().add(new JLabel(new ImageIcon(result)));
 		frame.pack();
 		frame.setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		Logger.getLogger("").setLevel(Level.WARNING);
+		new OfflineRenderApp(args);
 	}
 }
