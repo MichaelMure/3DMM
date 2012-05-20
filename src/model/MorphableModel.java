14a8c6e04478c3d328d37ca381eb7b8eedc956ad
Michael Muré <batolettre@gmail.com>
2012-05-20 22:50:29 +0900

    MorphableModel: add shortcut to directly update a GPU mesh from the MM

--- src/model/MorphableModel.java
+++ src/model/MorphableModel.java
@@ -1,9 +1,17 @@
 package model;
 
+import java.nio.FloatBuffer;
 import java.nio.IntBuffer;
+
+import cern.colt.matrix.DoubleMatrix1D;
+
+import com.jme3.scene.Mesh;
+import com.jme3.scene.VertexBuffer.Type;
 
 import pca.PCA;
 import pca.PCA_SVD;
+import util.Log;
+import util.Log.LogType;
 
 public class MorphableModel {
 
@@ -52,6 +60,37 @@ public class MorphableModel {
 		ensurePCA();
 		model.setVerticesMatrix(vertices.sampleToSampleSpace(param.getVerticesWeight()));
 		model.setColorMatrix(colors.sampleToSampleSpace(param.getColorWeight()));
+	}
+
+	/** Unconditional update of a mesh with the given parameters.
+	 *  This feed the GPU with new vertices and textures data.
+	 */
+	public void updateMesh(Mesh mesh, ModelParameter param) {
+		ensurePCA();
+		Log.info(LogType.MODEL, "Update mesh.");
+
+		FloatBuffer verticesBuffer = (FloatBuffer) mesh.getBuffer(Type.Position).getData();
+		FloatBuffer colorsBuffer = (FloatBuffer) mesh.getBuffer(Type.Color).getData();
+
+		verticesBuffer.clear();
+		colorsBuffer.clear();
+
+		/* TODO: non efficient: only one copy should be needed. */
+		DoubleMatrix1D newVertices = vertices.sampleToSampleSpace(param.getVerticesWeight());
+		DoubleMatrix1D newColors = colors.sampleToSampleSpace(param.getColorWeight());
+
+		for (int x = 0; x < newVertices.size(); x++) {
+			verticesBuffer.put((float) newVertices.getQuick(x));
+		}
+
+		for (int x = 0; x < newColors.size(); x++) {
+			colorsBuffer.put((float) newColors.getQuick(x));
+		}
+
+		mesh.setBuffer(Type.Position, 3, verticesBuffer);
+		mesh.setBuffer(Type.Color, 3, colorsBuffer);
+		mesh.updateCounts();
+		mesh.updateBound();
 	}
 
 	public int getSize() {
