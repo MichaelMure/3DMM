b962f5cf24fc30e7116ee183fea5cebcba0d46bf
Michael Muré <batolettre@gmail.com>
2012-06-18 19:39:46 +0900

    add two class to construct UI for model and render parameters

---
+++ src/editor/ModelParameterUI.java
@@ -0,0 +1,27 @@
+package editor;
+
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import parameter.ModelParameter;
+
+public class ModelParameterUI extends JPanel {
+
+	private static final long serialVersionUID = 7252416861944924355L;
+
+	public ModelParameterUI(ModelParameter param) {
+		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		this.add(new JLabel("Model parameters:"));
+
+		int modelCount = ModelParameter.getModelCount();
+
+		for(int i = 0; i < modelCount; i++) {
+			this.add(new Slider(param, i));
+		}
+
+		for(int i = 0; i < modelCount; i++) {
+			this.add(new Slider(param, modelCount + i));
+		}
+	}
+}
---
+++ src/editor/RenderParameterUI.java
@@ -0,0 +1,21 @@
+package editor;
+
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import parameter.RenderParameter;
+
+public class RenderParameterUI extends JPanel {
+
+	private static final long serialVersionUID = -1206184947044995069L;
+
+	public RenderParameterUI(RenderParameter param) {
+		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		this.add(new JLabel("Render parameters:"));
+
+		for(int i = 0; i <= RenderParameter.LAST_PARAM; i++) {
+			this.add(new Slider(param, i));
+		}
+	}
+}
