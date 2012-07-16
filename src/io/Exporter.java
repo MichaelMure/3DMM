196672a918533bfe5f4bdfd8557194722bae134f
Michael Muré <batolettre@gmail.com>
2012-07-16 16:38:41 +0900

    Exporter: better naming

--- src/io/Exporter.java
+++ src/io/Exporter.java
@@ -10,21 +10,21 @@ import util.Log.LogType;
 
 public abstract class Exporter {
 
-	public void exportObject(Mesh object, String filename) {
+	public void exportObject(Mesh mesh, String filename) {
 		File file = new File(filename);
 
-		exportObject(object, file);
+		exportObject(mesh, file);
 	}
 
-	public void exportObject(Mesh object, File file) {
+	public void exportObject(Mesh mesh, File file) {
 		Log.debug(LogType.IO, "Exporting: " + file.getName());
 
 		try {
-			doExportObject(object, file);
+			doExportObject(mesh, file);
 		} catch (IOException e) {
 			Log.error(LogType.IO, "Exporter IO error for " + file + ": " + e.getMessage());
 		}
 	}
 
-	protected abstract void doExportObject(Mesh object, File file) throws IOException;
+	protected abstract void doExportObject(Mesh mesh, File file) throws IOException;
 }
